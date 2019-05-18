3c7552007b63e500cb3a124a5a679d38fcf4f38f
Vladimir <vladimir.vkv@gmail.com>
2019-05-18 21:48:59 +0300

    Server: add filters method

--- server/routes/companies.js
+++ server/routes/companies.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router();
 const { getViewParamsFromQuery } = require('./helpers');
 const { getCompanies } = require('../data/dataHelpers');
+const FILTERS_STUB = ["Excavation", "Plumbing", "Electrical"];
 
 
 router.get('/', function(req, res) {
   const viewParams = getViewParamsFromQuery(req.query);
   const companies = getCompanies(viewParams);
   res.json(companies);
+});
+
+router.get('/filters', function(req, res) {
+  res.json(FILTERS_STUB);
 });
 
 module.exports = router;
