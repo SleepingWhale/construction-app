4743627608dd2b779b5fade2709665925594b3d1
Vladimir <vladimir.vkv@gmail.com>
2019-05-18 23:06:17 +0300

    Server: Add query string validation

--- server/routes/companies.js
+++ server/routes/companies.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const { query, validationResult } = require('express-validator/check');
 const { getViewParamsFromQuery } = require('./helpers');
 const { getCompanies } = require('../data/dataHelpers');
+
 const FILTERS_STUB = ["Excavation", "Plumbing", "Electrical"];
+const companiesQueryValidators = [
+  query('search').optional().isString(),
+  query('filters').optional().isArray(),
+  query('count').optional().isNumeric(),
+  query('page').optional().isNumeric(),
+];
 
 
-router.get('/', function(req, res) {
+
+router.get('/', companiesQueryValidators, function(req, res) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
   const viewParams = getViewParamsFromQuery(req.query);
   const companies = getCompanies(viewParams);
   res.json(companies);
