e85d1d08e2b87552c09fcd0dab47cb6172781693
Vladimir <vladimir.vkv@gmail.com>
2019-05-18 14:25:26 +0300

    Server: update default count, update mocked data

--- server/routes/helpers.js
+++ server/routes/helpers.js
@@ -1,15 +1,17 @@
+const MAX_ITEMS_PER_PAGE = 200;
+
 exports.getViewParamsFromQuery = function getViewParamsFromQuery(query) {
   const {
     search = '',
     filters = [],
     page = 0,
-    count = 200,
+    count = 50,
   } = query;
 
   return {
     search: search.toLowerCase(),
     filters: filters.filter((f) => f.length > 0),
     page: parseInt(page, 10) || 0,
-    count: parseInt(count, 10) || 0,
+    count: Math.min(parseInt(count, 10) || 10, MAX_ITEMS_PER_PAGE),
   }
 };
